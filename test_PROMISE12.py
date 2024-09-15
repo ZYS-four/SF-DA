f6d2a4468f83199a9384b333900f0c61044582eb
ZYS-four <1123258534@qq.com>
2024-09-15 16:43:28 +0800

    Update test_PROMISE12.py

--- test_PROMISE12.py
+++ test_PROMISE12.py
@@ -15,7 +15,7 @@ from tqdm import tqdm
 from networks.net_factory import net_factory
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--root_path', type=str, default='/home/eda/ZYSD/PROMISE12', help='Name of Experiment')
+parser.add_argument('--root_path', type=str, default='../ZYSD/PROMISE12', help='Name of Experiment')
 parser.add_argument('--exp', type=str, default='SF-DA', help='experiment_name')
 parser.add_argument('--model', type=str, default='unet', help='model_name')
 parser.add_argument('--num_classes', type=int,  default=2, help='output channel of network')
